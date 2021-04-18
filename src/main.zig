069cf19b481bcd053700007e4ce55e824fe1753c
pfg <pfg@pfg.pw>
2021-04-18 07:09:58 -0700

    unused structures that were manually copied and are hopefully right

--- src/main.zig
+++ src/main.zig
@@ -651,6 +651,41 @@ const sd = struct {
 };
 
 const boot_fs = struct {
+    const BiosParameterBlock = packed struct {
+        jmp: [3]u8,
+        oem: [8]u8,
+        bps0: u8,
+        bps1: u8,
+        spc: u8,
+        rsc: u16,
+        nf: u8,
+        nr0: u8,
+        nr1: u8,
+        ts16: u16,
+        media: u8,
+        spf16: u16,
+        spt: u16,
+        nh: u16,
+        hs: u32,
+        ts32: u32,
+        spf32: u32,
+        flg: u32,
+        rc: u32,
+        vol: [6]u8,
+        fst: [8]u8,
+        dmy: [20]u8,
+        fst2: [8]u8,
+    };
+    const FatDir = packed struct {
+        name: [8]u8,
+        ext: [3]u8,
+        /// [0]: packed struct {readonly: bool, hidden: bool, system: bool, volume_label: bool, directory: bool, archive: bool}
+        attr: [9]u8,
+        ch: u16,
+        attr2: u32,
+        c1: u16,
+        size: u32,
+    };
     const Partition = struct {
         lba: u32,
     };
