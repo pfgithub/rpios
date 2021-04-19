ffab6b6a968bc1706a32ed520baba637cc1ed92a
pfg <pfg@pfg.pw>
2021-04-19 08:44:45 -0700

    clear bss because c expects it apparently

--- src/boot.s
+++ src/boot.s
@@ -43,12 +43,12 @@ _start:
     mov     sp, x1
 
     // clear bss
-//    ldr     x1, =__bss_start
-//    ldr     w2, =__bss_size
-//3:  cbz     w2, 4f
-//    str     xzr, [x1], #8
-//    sub     w2, w2, #1
-//    cbnz    w2, 3b
+    ldr     x1, =__bss_start
+    ldr     w2, =__bss_size
+3:  cbz     w2, 4f
+    str     xzr, [x1], #8
+    sub     w2, w2, #1
+    cbnz    w2, 3b
     // nah who needs this.
 
     // jump to C code, should not return
